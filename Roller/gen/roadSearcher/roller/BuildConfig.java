df0b44e37283f3a07d4f6d9883cf7d40015d078e
Bjørn Dølvik <bjorndolvik@gmail.com>
2013-08-30 19:10:54 +0200

    First functional version
    Simple button gui, with all the required methods and roll generator

---
+++ Roller/gen/roadSearcher/roller/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package roadSearcher.roller;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Roller/src/roadSearcher/fragments/DiceThrowerFragment.java
@@ -0,0 +1,18 @@
+package roadSearcher.fragments;
+
+import roadSearcher.roller.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DiceThrowerFragment extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.dice_thrower, container, false);
+	}
+}
